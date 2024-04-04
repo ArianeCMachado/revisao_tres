72a2717144b22db7a1c8f6117295a153fd7a6b8a
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2024-04-04 20:28:03 -0300

    Resolve questão 2

--- array_e_indice.py
+++ array_e_indice.py
@@ -1,9 +1,16 @@
-def imprimir_nomes():
-    nomes = ["João", "Maria", "Fulano", "Ciclano"]
+nomes = ["João", "Maria", "Fulano", "Ciclano"]
 
+def imprimir_nomes():
     print("1 -", nomes[0])
     print("2 -", nomes[1])
     print("3 -", nomes[2])
     print("4 -", nomes[3])
 
 imprimir_nomes()
+
+
+def imprime_primeiro_ultimo():
+    print("Primeiro -", nomes[0])
+    print("Último -", nomes[3])
+
+imprime_primeiro_ultimo()
