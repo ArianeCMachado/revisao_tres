28000e309c39e06659f8dfec3289593339a8f7af
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2024-04-04 21:47:57 -0300

    Resolve questão 8

--- loops.py
+++ loops.py
@@ -22,6 +22,15 @@ contar_ate_numero()
 
 
 def imprime_tabuada_dois():
-    for i in range(0,20,2):
-        print(i)
+    for i in range(11):
+        print(f"2 + {i} = {2 + i}")
+
 imprime_tabuada_dois()
+
+
+def tabuada_user():
+    numero = int(input("Qual a tabuada desejada?"))
+    for i in range(0,11):
+        print(f"{numero} x {i} = {numero * i}")
+
+tabuada_user()
