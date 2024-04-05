1098ec0bb87f95dbb2539328f7e2db0c51c596dd
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2024-04-04 21:36:29 -0300

    Resolve questão 7

--- loops.py
+++ loops.py
@@ -12,9 +12,16 @@ def contar_ate_vinte():
 
 contar_ate_vinte()
 
+
 def contar_ate_numero():
     numero = int(input("Digite um número inteiro: "))
     for i in range(numero + 1):
         print(i)
 
 contar_ate_numero()
+
+
+def imprime_tabuada_dois():
+    for i in range(0,20,2):
+        print(i)
+imprime_tabuada_dois()
